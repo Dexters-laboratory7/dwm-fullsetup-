976b80af29b34f677248577f1e91186029e4f602
Name <test@mail.com>
2020-08-24 10:02:45 +0530

    Minor ISSUe

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu vim xorg-server xorg-xinit xorg-xbacklight picom neofetch htop wget xorg-xrandr xorg-setxroot xf86-video-intel xwallpaper 
+sudo pacman -Syu vim xorg-server xorg-xinit xorg-xbacklight picom neofetch htop wget xorg-xrandr xorg-xsetroot xf86-video-intel xwallpaper 
 
 sudo cp 90-synaptics.conf /etc/X11/xorg.conf.d/.
 
