b6a582c34f67e65bc8d512c13dd41b6ee542ba14
dexters-laboratory7 <dexters-laboratory7@github.com>
2020-09-06 19:58:38 +0530

    added used packages

--- packages.sh
+++ packages.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu vim xorg-server xorg-xinit xorg-xbacklight picom neofetch htop wget xorg-xrandr xorg-xsetroot xf86-video-intel xwallpaper pulseaudio pavucontrol pulsemixer 
+sudo pacman -Syu vim xorg-server xorg-xinit xorg-xbacklight picom neofetch htop wget xorg-xrandr xorg-xsetroot xf86-video-intel xwallpaper pulseaudio pavucontrol pulsemixer ttf-font-awesome libao opusfile wavpack libao cmus ranger atool elinks ffmpegthumbnailer highlight libcaca mediainfo odt2txt perl-image-exiftool poppler python-chardet ueberzug
+
+cp xinitrc ~/.xinitrc
+
+cp vimrc ~/.vimrc
+
+cp -rf vim ~/.vim
+
+
 
 sudo cp 90-synaptics.conf /etc/X11/xorg.conf.d/.
 
--- scripts/time.sh
+++ scripts/time.sh
@@ -3,5 +3,5 @@
 
 while :;do
 	xsetroot -name "$(date) ||  Battery $(cat /sys/class/power_supply/BAT0/capacity)"
-	sleep 15
+	sleep 1
 done
